77a0f089a148383fc63fd0966878c0965e826a03
Nikhil Suresh <nik.suresh.ns@gmail.com>
2023-03-12 21:46:46 +1100

    Add solution for Day 8, Part 2

--- day_8/code/solution.py
+++ day_8/code/solution.py
@@ -68,3 +68,61 @@ for coordinate in coordinates_to_check:
         visible_trees -= 1
     
 print(f"Visible trees: {visible_trees}")
+
+#########
+# Part 2
+#########
+print("\nPart 2:")
+
+def trees_visible_from_coordinates(target_tree_height, coordinates_to_compare, tree_grid):
+    """Returns the number of trees visible from the target tree.
+
+    Arguments:
+        target_tree_height {int} -- The height of the tree we're checking.
+        coordinates_to_compare {list} -- A list of (x, y) tuples representing the coordinates of trees to compare against the target tree.
+        tree_grid {list} -- A list of lists representing the grid of trees.
+
+    Returns:
+        int -- The number of trees visible from the target tree.
+        """
+    trees_visible_from_target = 0
+    for (neighbour_x, neighbour_y) in coordinates_to_compare:
+        neighbouring_tree_height = tree_grid[neighbour_y][neighbour_x]
+        if neighbouring_tree_height < target_tree_height:
+            trees_visible_from_target += 1
+        elif neighbouring_tree_height >= target_tree_height:
+            trees_visible_from_target += 1
+            break
+    return trees_visible_from_target
+
+max_score = 0
+best_coordinate = None
+# Check each tree in the grid.
+for coordinate in coordinates_to_check:
+    x, y = coordinate
+    target_tree_height = tree_grid[y][x]
+
+    # Higher Y represents lower down the grid.
+    # Higher X represents further to the right on the grid.
+
+    # The coordinates will be iterated over in order of increasing Y.
+    # 
+    trees_in_line_above = [coord for coord in coordinates_to_check if coord[0] == x and coord[1] < y][::-1]
+    trees_in_line_below = [coord for coord in coordinates_to_check if coord[0] == x and coord[1] > y]
+
+    trees_in_line_to_left = [coord for coord in coordinates_to_check if coord[1] == y and coord[0] < x][::-1]
+    trees_in_line_to_right = [coord for coord in coordinates_to_check if coord[1] == y and coord[0] > x]
+
+    # Check if there are any trees in the lines above, below, to the left and to the right of the target tree.
+    number_of_visible_trees_above = trees_visible_from_coordinates(target_tree_height, trees_in_line_above, tree_grid)
+    number_of_visible_trees_below = trees_visible_from_coordinates(target_tree_height, trees_in_line_below, tree_grid)
+    number_of_visible_trees_to_left = trees_visible_from_coordinates(target_tree_height, trees_in_line_to_left, tree_grid)
+    number_of_visible_trees_to_right = trees_visible_from_coordinates(target_tree_height, trees_in_line_to_right, tree_grid)
+    
+    score = number_of_visible_trees_above * number_of_visible_trees_below * number_of_visible_trees_to_left * number_of_visible_trees_to_right
+    if score > max_score:
+        max_score = score
+        best_coordinate = coordinate
+
+print(f"Best coordinate: {best_coordinate}")
+print(f"Max score: {max_score}")
